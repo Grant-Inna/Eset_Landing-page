2cddce3524713686d8966b4ef2186960388110ff
Инна Грант <grant@SMS.SMS>
2017-05-18 16:45:16 +0300

    New refactor

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     cleanCSS = require('gulp-clean-css'),
     rename = require("gulp-rename"),
@@ -7,11 +6,10 @@ var gulp = require('gulp'),
 
 
 gulp.task('css', function() {
-    return gulp.src('./style.scss')
-        .pipe(sass().on('error', sass.logError)) // Turn scss file into css
+    return gulp.src('./style.css')
         .pipe(autoprefixer({browsers: ['last 5 versions', '> 5%']}))
         .pipe(gulp.dest('../css'))
-        .pipe(notify('CSS Success!'));
+        .pipe(notify('Autoprefixer Success!'));
 });
 
 gulp.task('minCss', function() {
@@ -22,14 +20,12 @@ gulp.task('minCss', function() {
         .pipe(notify('minCSS Success!'));
 });
 
-gulp.task('watch_scss', function() {
-    gulp.watch('./style.scss', ['css'])
-    gulp.watch('./_variables.scss', ['css'])
-    gulp.watch('./_media.scss', ['css'])
+gulp.task('watch_css', function() {
+    gulp.watch('./style.css', ['css'])
 });
 gulp.task('watch_min', function() {
     gulp.watch('../css/style.css', ['minCss'])
 });
 
 
-gulp.task('default', ['css', 'minCss', 'watch_min', 'watch_scss']);
+gulp.task('default', ['css', 'minCss', 'watch_min', 'watch_css']);
