ec3a5bcbdaa7a2970e6733c96d771fe8ba69e65a
Инна Грант <grant@SMS.SMS>
2017-05-19 14:25:32 +0300

    Small refactor

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -1,17 +1,25 @@
 var gulp = require('gulp'),
+    csscomb = require('gulp-csscomb'),
+    gcmq = require('gulp-group-css-media-queries'),
     autoprefixer = require('gulp-autoprefixer'),
     cleanCSS = require('gulp-clean-css'),
     rename = require("gulp-rename"),
     notify = require("gulp-notify");
 
 
-gulp.task('css', function() {
+gulp.task('cssComb', function() {
     return gulp.src('./style.css')
-        .pipe(autoprefixer({browsers: ['last 5 versions', '> 5%']}))
+        .pipe(csscomb())
+        .pipe(gulp.dest('./'))
+        .pipe(notify('cssComb Success!'));
+});
+gulp.task('autoprefixer', function() {
+    return gulp.src('./style.css')
+        .pipe(gcmq())
+        .pipe(autoprefixer({browsers: ['last 5 versions', '> 3%']}))
         .pipe(gulp.dest('../css'))
         .pipe(notify('Autoprefixer Success!'));
 });
-
 gulp.task('minCss', function() {
     return gulp.src('../css/style.css')
         .pipe(cleanCSS())
@@ -20,12 +28,15 @@ gulp.task('minCss', function() {
         .pipe(notify('minCSS Success!'));
 });
 
-gulp.task('watch_css', function() {
-    gulp.watch('./style.css', ['css'])
+gulp.task('watch_cssComb', function() {
+    gulp.watch('./style.css', ['cssComb'])
+});
+gulp.task('watch_autoprefixer', function() {
+    gulp.watch('./style.css', ['autoprefixer'])
 });
 gulp.task('watch_min', function() {
     gulp.watch('../css/style.css', ['minCss'])
 });
 
 
-gulp.task('default', ['css', 'minCss', 'watch_min', 'watch_css']);
+gulp.task('default', ['cssComb', 'autoprefixer', 'minCss', 'watch_min', 'watch_autoprefixer', 'watch_cssComb']);
